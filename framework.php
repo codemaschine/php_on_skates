74d749a96aa141ec8e886c44a9d5d9d188c8b553
Micha Rosenbaum <micha@rosetree.de>
2015-09-18 19:56:22 +0200

    Remove Trailing Whitespace

--- framework.php
+++ framework.php
@@ -3,14 +3,14 @@
 $abs_base_path = dirname(__FILE__);
 chdir('app');
 
-  
+
 // ---- Do init -----
 $_FRAMEWORK = array();
 if ($_GET['frameworkController']) {
   $_FRAMEWORK['controller'] = $_GET['frameworkController'];
   unset($_GET['frameworkController']);
 }
-  
+
 
 
 require 'commons/init.php';
@@ -27,7 +27,7 @@ try {
         else
           $_FRAMEWORK['controller'] = '404.php';
       }
-    
+
       require 'commons/pre_controller.php';
       if ($_FRAMEWORK['redirect'] || $_FRAMEWORK['render_type'])  // abort if redirect() or render() is called. Don't go into the controller
         break;
@@ -35,16 +35,16 @@ try {
       require 'commons/post_controller.php';
       if ($_FRAMEWORK['redirect'])
         break;
-      
+
     }
     while ($_FRAMEWORK['forward']);
   }
-    
-    
+
+
   // ---- Do Redirect or load view
   if ($_FRAMEWORK['redirect']) {
     require 'commons/post_rendering.php';
-    
+
     $uri = substr($_FRAMEWORK['redirect_to'], 0, 4) == 'http' ? $_FRAMEWORK['redirect_to'] : $c_base_url.$_FRAMEWORK['redirect_to'];
     if (is_json()) {
     	$_FRAMEWORK['is_rendering'] = true;
@@ -52,22 +52,22 @@ try {
     }
     else
       header("Location: ".$uri);
-      
-      
+
+
     $fwlog->info("Redirect to ".$uri);
     exit();
   }
   else {
     if ($_FRAMEWORK['format'] == 'json') // At json-requests always return 200, because the status of request ist handled inside of the JSON-object!
       $_FRAMEWORK['status_code'] = 200;
-    
+
     if ($_FRAMEWORK['status_code'] != 200)
       header("HTTP/1.1 ".$_FRAMEWORK['status_code'].' USER ERROR');
-    
-    
+
+
     $header_type = 'text';
     $header_subtype = 'plain';
-    
+
     switch ($_FRAMEWORK['format']) {
       case 'xml': $header_subtype = 'xml'; break;
       case 'js': $header_subtype = 'javascript'; break;
@@ -75,44 +75,44 @@ try {
       case 'htm':
       case 'html': $header_subtype = 'html'; break;
       case 'css': $header_subtype = 'css'; break;
-      case 'json': $header_type = 'application'; $header_subtype = 'json'; 
+      case 'json': $header_type = 'application'; $header_subtype = 'json';
         if (!$_FRAMEWORK['render_type'] || $_FRAMEWORK['render_type'] != 'json') {
         	render_json(response_with(null, 501, 'JSON-Renderer not called in Controller! This call might not be implemented.'));
         }
         break;
     }
-    
+
     header("Content-Type: $header_type/$header_subtype; charset=utf-8");
-    
+
     // security check: is it allowed and possible to render this file?
     if ($_FRAMEWORK['render_type'] != 'text') {
       //if (strpos($_FRAMEWORK['view'], '.') === false)
       //  $_FRAMEWORK['view'] .= '.php';
-      
+
       if (strpos($_FRAMEWORK['view'], '/') === false) // add controller path if not specified
         $_FRAMEWORK['view'] = substr($_FRAMEWORK['controller'], 0, strrpos($_FRAMEWORK['controller'], '.')).'/'.$_FRAMEWORK['view'];
-      
+
       //$_FRAMEWORK['view'] = str_replace('../', '', $_FRAMEWORK['view']);
       //if (!file_exists('views/'.$_FRAMEWORK['view'])) {
       //  throw new ErrorException("View {$_FRAMEWORK['view']} does not exist!");
       //}
     }
     //  ________________
-    
+
     //  adapt layout and view according to format
     if (substr($_FRAMEWORK['layout'], -4) == '.php')
       $_FRAMEWORK['layout'] = substr($_FRAMEWORK['layout'], 0, -4);
-    
+
     if ($_FRAMEWORK['format'] != 'php')
       $_FRAMEWORK['layout'] .= '.'.$_FRAMEWORK['format'];
-    
+
     $_FRAMEWORK['layout'] .= '.php';
-    
-    
+
+
     //  ________________
-    
+
     $echoLock=0;
-    
+
     if ($_FRAMEWORK['render_type'] == 'text') {
       $fwlog->info('Render '.($_FRAMEWORK['render_content'] ? 'Text' : 'Nothing (except flash-messages if XHR)').', Code '.$_FRAMEWORK['status_code']);
       echo $_FRAMEWORK['render_content'];
@@ -132,29 +132,29 @@ try {
         $_FRAMEWORK['is_rendering'] = true;
         render($_FRAMEWORK['view']);
       }
-      show_flash(); // dump the flash messages, if they aren't dumped yet. Usefull for bugfixing, but it can also be turned off 
+      show_flash(); // dump the flash messages, if they aren't dumped yet. Usefull for bugfixing, but it can also be turned off
     }
     else {
       $fwlog->info('Render View "'.$_FRAMEWORK['view'].'", Format: '.$_FRAMEWORK['format'].', Code '.$_FRAMEWORK['status_code']);
-      
-      
-      
+
+
+
       $_FRAMEWORK['is_layouting'] = true;
       include 'views/layout/'.$_FRAMEWORK['layout']; // this also renders the view by calling yieldit();
-      
+
       /*
       if ($debug) {
         echo '<div class="debug">FRAMEWORK: ';
         var_dump($_FRAMEWORK, true);
         echo '</div>';
-        
+
       }*/
-      
+
     }
-    
+
     require 'commons/post_rendering.php';
   }
-    
+
 } catch (ErrorException $e) {
   $log->error("{$e->getMessage()} in {$e->getFile()} on line {$e->getLine()}\n{$e->getTraceAsString()}");
   $fwlog->error("{$e->getMessage()} in {$e->getFile()} on line {$e->getLine()}\n{$e->getTraceAsString()}");
