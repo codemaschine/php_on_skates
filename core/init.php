f8b90fbb25400913213ce7c06aa8b58a8cdac0f4
Leon Wolfgang Rönnpagel <leworoe@codemaschine.de>
2024-04-16 17:43:57 +0200

    Added option for no database

--- core/init.php
+++ core/init.php
@@ -268,7 +268,8 @@ require_once 'locale.php';
 require_once 'attachment.php';
 require_once 'base.php';
 require_once 'db.php';
-$db_link = db_init();
+if (!empty($db_config))
+  $db_link = db_init();
 require_once 'abstract_model.php';
 require_once 'relations.php';
 require_once 'functions.php';
